cc245369f7907f4444d4b06c4091b2c20b5017fb
Ben <benjamin.etheredge@gmail.com>
2021-06-21 06:10:01 +0000

    Reworking setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,26 @@
-from setuptools import setup, find_packages
+import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
-setup(
+setuptools.setup(
     name="pokegen",
     version="0.0.1",
-    packages=find_packages(),
+    author="Benjamin Etheredge",
+    #author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    #url="https://github.com/pypa/sampleproject",
+    #project_urls={
+        #"Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    #},
+    #classifiers=[
+        #"Programming Language :: Python :: 3",
+        #"License :: OSI Approved :: MIT License",
+        #"Operating System :: OS Independent",
+    #],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8",
 )
