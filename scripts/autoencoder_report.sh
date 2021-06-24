83ae8a33ee4b86c838aa92357ad5a26e879a8a94
Ben <benjamin.etheredge@gmail.com>
2021-06-24 23:36:23 +0000

    Pulling up some logic and adding VAE

--- scripts/autoencoder_report.sh
+++ scripts/autoencoder_report.sh
@@ -1,4 +1,16 @@
-version=$1
+model=$1
+version=$2
+
+echo "# $model ($version)"
+
 echo "## Metrics"
 git fetch --prune >& /dev/null
-dvc metrics diff main --target reports/autoencoder/$version/logs.json --show-md
+dvc metrics diff main --target reports/$model/$version/logs.json --show-md
+
+echo "## Generated"
+gen_dir=reports/$model/$version/gen
+for f in $(ls $gen_dir/*.gif)
+do
+    echo "Publishing $f"
+    cml-publish "$f" --md 
+done
