f3c13428b83d193f2386929eec2bfb15d103bfc9
Benjamin Etheredge <Benjamin-Etheredge@users.noreply.github.com>
2021-06-24 20:36:40 -0500

    Fixing missing for

--- scripts/autoencoder_report.sh
+++ scripts/autoencoder_report.sh
@@ -8,7 +8,8 @@ git fetch --prune >& /dev/null
 dvc metrics diff main --target reports/$model/$version/logs.json --show-md
 
 echo "## Encoded/Decoded"
-gen_dir=reports/$model/$version/results
+results_dir=reports/$model/$version/results
+for f in $(ls $results_dir/*gif)
 do
     cml-publish "$f/raw.jpg" --md 
     cml-publish "$f/result.jpg" --md 
@@ -17,6 +18,7 @@ done
 
 echo "## Generated"
 gen_dir=reports/$model/$version/gen
+for f in $(ls $gen_dir/*gif)
 do
     cml-publish "$f" --md 
 done
