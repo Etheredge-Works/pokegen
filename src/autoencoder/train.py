6319e33e44a14db5d1ac8fde1265a2f7345ec676
Ben <benjamin.etheredge@gmail.com>
2021-06-27 05:18:17 +0000

    Removing TQDM from github action runs

--- src/autoencoder/train.py
+++ src/autoencoder/train.py
@@ -22,7 +22,7 @@ def train_ae(
     trainloader: DataLoader,
     ae: torch.nn.Module,
     lr=0.001,
-    gen_log_freq=20,
+    should_tqdm=os.getenv('SHOULD_TQDM', 1)  # using env for github action running 
 ):
     log_dir = Path(log_dir)
     gen_dir = log_dir/'gen'
@@ -58,7 +58,11 @@ def train_ae(
         running_loss = 0
         total = 0 # use total as drop_last=True
         ae.train()
-        for image_b in tqdm(trainloader):
+        if should_tqdm:
+            iter_trainloader = tqdm(trainloader)
+        else:
+            iter_trainloader = trainloader
+        for image_b in iter_trainloader:
             #print(data[0])
             image_b = image_b.to(device)
             y_pred = ae(image_b)
