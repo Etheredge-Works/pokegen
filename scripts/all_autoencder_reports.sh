c0f0a14914154dee5a7aabc4e87d3fa11c98befb
Ben <benjamin.etheredge@gmail.com>
2021-06-27 19:58:54 +0000

    Fixing name issue

--- scripts/all_autoencder_reports.sh
+++ scripts/all_autoencder_reports.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 echo "# Report" 
 dvc params diff main --show-md
-./scripts/autoencoder_report.sh autoencoder dense
-./scripts/autoencoder_report.sh autoencoder conv 
+./scripts/autoencoder_report.sh ae dense
+./scripts/autoencoder_report.sh ae conv 
 ./scripts/autoencoder_report.sh vae dense
 ./scripts/autoencoder_report.sh vae conv
