6fd9860aca3b0cd6a9cabe91028865ed77853327
Benjamin Etheredge <benjamin.etheredge@gmail.com>
2021-08-04 19:08:06 +0000

    Testing using head in params diff

--- scripts/all_autoencoder_reports.sh
+++ scripts/all_autoencoder_reports.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 echo "# Report" 
-dvc params diff main --show-md
+dvc params diff main HEAD --show-md
 ./scripts/autoencoder_report.sh ae dense
 ./scripts/autoencoder_report.sh ae conv 
 ./scripts/autoencoder_report.sh vae dense
