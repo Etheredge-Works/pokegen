05e5f839f4ec79d6c8a07c7746c00ef5e45cddc9
Ben <benjamin.etheredge@gmail.com>
2022-02-13 08:06:01 +0000

    Fixing conv bug. Removed batch norm but didn't add back bias.

--- src/autoencoder/encoders/conv.py
+++ src/autoencoder/encoders/conv.py
@@ -20,44 +20,44 @@ class ConvEncoder(torch.nn.Module):
         # TODO enlarge kernel
         conv_layers = [
             (
-                nn.Conv2d(input_shape[0], 16, 3, stride=1, padding=1, bias=False),
-                nn.BatchNorm2d(16)
+                nn.Conv2d(input_shape[0], 16, 3, stride=1, padding=1, bias=True),
+                #nn.BatchNorm2d(16)
             ),
             (
-                nn.Conv2d(16, 16, 3, stride=1, padding=1, bias=False),
-                nn.BatchNorm2d(16)
+                nn.Conv2d(16, 16, 3, stride=1, padding=1, bias=True),
+                #nn.BatchNorm2d(16)
             ),
             (
-                nn.Conv2d(16, 32, 3, stride=2, padding=1, bias=False),
-                nn.BatchNorm2d(32)
+                nn.Conv2d(16, 32, 3, stride=2, padding=1, bias=True),
+                #nn.BatchNorm2d(32)
             ),
             (
-                nn.Conv2d(32, 32, 3, stride=1, padding=1, bias=False),
-                nn.BatchNorm2d(32)
+                nn.Conv2d(32, 32, 3, stride=1, padding=1, bias=True),
+                #nn.BatchNorm2d(32)
             ),
             (
-                nn.Conv2d(32, 64, 3, stride=2, padding=1, bias=False),
-                nn.BatchNorm2d(64)
+                nn.Conv2d(32, 64, 3, stride=2, padding=1, bias=True),
+                #nn.BatchNorm2d(64)
             ),
             (
-                nn.Conv2d(64, 64, 3, stride=1, padding=1, bias=False),
-                nn.BatchNorm2d(64)
+                nn.Conv2d(64, 64, 3, stride=1, padding=1, bias=True),
+                #nn.BatchNorm2d(64)
             ),
             (
-                nn.Conv2d(64, 128, 3, stride=2, padding=1, bias=False),
-                nn.BatchNorm2d(128)
+                nn.Conv2d(64, 128, 3, stride=2, padding=1, bias=True),
+                #nn.BatchNorm2d(128)
             ),
             (
-                nn.Conv2d(128, 128, 3, stride=1, padding=1, bias=False),
-                nn.BatchNorm2d(128)
+                nn.Conv2d(128, 128, 3, stride=1, padding=1, bias=True),
+                #nn.BatchNorm2d(128)
             ),
             (
-                nn.Conv2d(128, 256, 3, stride=2, padding=1, bias=False),
-                nn.BatchNorm2d(256)
+                nn.Conv2d(128, 256, 3, stride=2, padding=1, bias=True),
+                #nn.BatchNorm2d(256)
             ),
             (
-                nn.Conv2d(256, 256, 3, stride=1, padding=1, bias=False),
-                nn.BatchNorm2d(256)
+                nn.Conv2d(256, 256, 3, stride=1, padding=1, bias=True),
+                #nn.BatchNorm2d(256)
             ),
             #(
                 #nn.Conv2d(256, 512, 3, stride=2, padding=1, bias=False),
