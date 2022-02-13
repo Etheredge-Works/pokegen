530ca9eef4b27f5e6a1a61f2a6730c0db5960cf0
Ben <benjamin.etheredge@gmail.com>
2022-02-13 08:08:04 +0000

    Fixing bug.

--- src/autoencoder/encoders/conv.py
+++ src/autoencoder/encoders/conv.py
@@ -21,43 +21,43 @@ class ConvEncoder(torch.nn.Module):
         conv_layers = [
             (
                 nn.Conv2d(input_shape[0], 16, 3, stride=1, padding=1, bias=True),
-                #nn.BatchNorm2d(16)
+                nn.BatchNorm2d(16)
             ),
             (
                 nn.Conv2d(16, 16, 3, stride=1, padding=1, bias=True),
-                #nn.BatchNorm2d(16)
+                nn.BatchNorm2d(16)
             ),
             (
                 nn.Conv2d(16, 32, 3, stride=2, padding=1, bias=True),
-                #nn.BatchNorm2d(32)
+                nn.BatchNorm2d(32)
             ),
             (
                 nn.Conv2d(32, 32, 3, stride=1, padding=1, bias=True),
-                #nn.BatchNorm2d(32)
+                nn.BatchNorm2d(32)
             ),
             (
                 nn.Conv2d(32, 64, 3, stride=2, padding=1, bias=True),
-                #nn.BatchNorm2d(64)
+                nn.BatchNorm2d(64)
             ),
             (
                 nn.Conv2d(64, 64, 3, stride=1, padding=1, bias=True),
-                #nn.BatchNorm2d(64)
+                nn.BatchNorm2d(64)
             ),
             (
                 nn.Conv2d(64, 128, 3, stride=2, padding=1, bias=True),
-                #nn.BatchNorm2d(128)
+                nn.BatchNorm2d(128)
             ),
             (
                 nn.Conv2d(128, 128, 3, stride=1, padding=1, bias=True),
-                #nn.BatchNorm2d(128)
+                nn.BatchNorm2d(128)
             ),
             (
                 nn.Conv2d(128, 256, 3, stride=2, padding=1, bias=True),
-                #nn.BatchNorm2d(256)
+                nn.BatchNorm2d(256)
             ),
             (
                 nn.Conv2d(256, 256, 3, stride=1, padding=1, bias=True),
-                #nn.BatchNorm2d(256)
+                nn.BatchNorm2d(256)
             ),
             #(
                 #nn.Conv2d(256, 512, 3, stride=2, padding=1, bias=False),
