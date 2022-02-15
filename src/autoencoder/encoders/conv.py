b21547a49d742b904b0c8133dfa10c40740b1f1e
Ben <benjamin.etheredge@gmail.com>
2022-02-15 07:05:59 +0000

    Exploring checkerboarding. https://distill.pub/2016/deconv-checkerboard/

--- src/autoencoder/encoders/conv.py
+++ src/autoencoder/encoders/conv.py
@@ -28,7 +28,7 @@ class ConvEncoder(torch.nn.Module):
             #     nn.BatchNorm2d(16)
             # ),
             (
-                nn.Conv2d(16, 32, 3, stride=2, padding=1, bias=True),
+                nn.Conv2d(16, 32, 4, stride=2, padding=1, bias=True),
                 nn.BatchNorm2d(32)
             ),
             # (
@@ -36,7 +36,7 @@ class ConvEncoder(torch.nn.Module):
             #     nn.BatchNorm2d(32)
             # ),
             (
-                nn.Conv2d(32, 64, 3, stride=2, padding=1, bias=True),
+                nn.Conv2d(32, 64, 4, stride=2, padding=1, bias=True),
                 nn.BatchNorm2d(64)
             ),
             # (
@@ -44,7 +44,7 @@ class ConvEncoder(torch.nn.Module):
             #     nn.BatchNorm2d(64)
             # ),
             (
-                nn.Conv2d(64, 128, 3, stride=2, padding=1, bias=True),
+                nn.Conv2d(64, 128, 4, stride=2, padding=1, bias=True),
                 nn.BatchNorm2d(128)
             ),
             # (
@@ -52,7 +52,7 @@ class ConvEncoder(torch.nn.Module):
             #     nn.BatchNorm2d(128)
             # ),
             (
-                nn.Conv2d(128, 256, 3, stride=2, padding=1, bias=True),
+                nn.Conv2d(128, 256, 4, stride=2, padding=1, bias=True),
                 nn.BatchNorm2d(256)
             ),
             # (
